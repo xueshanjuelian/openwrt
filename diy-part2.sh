af68f846cffa1e5800c2ffab5ce197c055be053d
xueshanjuelian <xueshanjuelian@163.com>
2024-05-26 09:11:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.9.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 sed -i 's/OpenWrt/HuiYui-Router/g' package/base-files/files/bin/config_generate
